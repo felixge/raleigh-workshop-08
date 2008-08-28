e6f20e6b01d155fa67bd8e7a58db8c59eb3ad88c
Felix Geisendörfer <felix@debuggable.com>
2008-08-28 18:39:18 +0200

    Refactored cloud function, I

--- application/app/models/command.php
+++ application/app/models/command.php
@@ -20,15 +20,18 @@ class Command extends AppModel {
 	function find($type, $query = array()) {
 		switch ($type) {
 			case 'cloud':
-				$limit = 30;
-				$order = array('Command.snippet_command_count' => 'desc');
-				$contain = false;
-				$commands = $this->find('all', compact('contain', 'order'));
+				$options = array_merge(array(
+					'limit' => 30,
+					'maxSize' => 200,
+					'minSize' => 90
+				), $query);
+			
+				$commands = $this->find('all', array(
+					'contain' => false,
+					'order' => array('Command.snippet_command_count' => 'desc'),
+				));
 
-				$maxSize = 200;
-				$minSize = 90;
-
-				$resultCount = ($limit > count($commands)) ? count($commands) : $limit;
+				$resultCount = ($options['limit'] > count($commands)) ? count($commands) : $options['limit'];
 				$maxCount = @$commands[0]['Command']['snippet_command_count'];
 				$minCount = @$commands[$resultCount-1]['Command']['snippet_command_count'];
 
@@ -42,8 +45,8 @@ class Command extends AppModel {
 				shuffle($commands);
 				$output = array(
 					'Command' => $commands,
-					'minSize' => $minSize,
-					'maxSize' => $maxSize,
+					'minSize' => $options['minSize'],
+					'maxSize' => $options['maxSize'],
 					'minCount' => $minCount,
 					'maxCount' => $maxCount,
 					'spread' => $spread,
